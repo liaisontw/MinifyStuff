8a212203b03eaa2ae8e1f6163c3f2c9db54df1c9
liaisontw <liaison.tw@gmail.com>
2025-06-05 16:29:02 +0800

    first commit

---
+++ includes/class-minifystuff-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/liaisontw/
+ * @since      1.0.0
+ *
+ * @package    minifyStuff
+ * @subpackage minifyStuff/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    minifyStuff
+ * @subpackage minifyStuff/includes
+ * @author     Liaison Chang <liaison.tw@gmail.com>
+ */
+class minifyStuff_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-minifystuff-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/liaisontw/
+ * @since      1.0.0
+ *
+ * @package    minifyStuff
+ * @subpackage minifyStuff/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    minifyStuff
+ * @subpackage minifyStuff/includes
+ * @author     Liaison Chang <liaison.tw@gmail.com>
+ */
+class minifyStuff_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
